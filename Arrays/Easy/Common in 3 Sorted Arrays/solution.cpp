0e5354eb8597e34d3a8e5313b94b5b90b2e118dc
ankushkumar273 <ankushkumar12102003@gmail.com>
2026-08-14 10:15:22 +0530

    Solved: Common in 3 Sorted Arrays [Easy]

---
+++ Arrays/Easy/Common in 3 Sorted Arrays/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+        // code here
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        int i=0; int j=0; int k=0;
+        
+        while(i<a.length &&
+        j<b.length &&
+        k<c.length){
+            
+            if(a[i]==b[j] && b[j]==c[k]){
+                
+                //dublicat ignore karo
+                
+                if(result.isEmpty() || result.get(result.size() -1 ) != a[i]){
+                    result.add(a[i]);
+                }
+                i++;
+                j++;
+                k++;
+                  
+            }
+                
+                else if(a[i] < b[j]){
+                    i++;
+                }
+                
+                else if(b[j] < c[k]){
+                    j++;
+                }
+                
+                else{
+                    k++;
+                }
+            
+            
+        }
+        return result;
+    }
+}
