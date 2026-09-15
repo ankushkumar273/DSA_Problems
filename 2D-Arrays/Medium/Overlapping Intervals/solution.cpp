25c5a642562eacd1492a78f0c7b2837fca2dc9c8
ankushkumar273 <ankushkumar12102003@gmail.com>
2026-09-15 23:02:23 +0530

    Solved: Overlapping Intervals [Medium]

---
+++ 2D-Arrays/Medium/Overlapping Intervals/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> mergeOverlap(int[][] arr) {
+        // Code here
+        Arrays.sort(arr, (a,b) -> a[0]-b[0]);
+        int start = arr[0][0];
+        int end = arr[0][1];
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        
+        for(int i=1; i<arr.length; i++){
+            int s = arr[i][0];
+            int e = arr[i][1];
+            
+            if(s <= end){
+                end = Math.max(e,end);
+            }else{
+                list.add(new ArrayList<>(Arrays.asList(start, end)));
+                start = s;
+                end = e;
+            }
+        }
+        list.add(new ArrayList<>(Arrays.asList(start, end)));
+        return list;
+    }
+}
