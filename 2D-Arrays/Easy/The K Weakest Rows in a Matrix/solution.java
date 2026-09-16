c3f42d9a397e18807cb0b12b6aac606434774036
ankushkumar273 <ankushkumar12102003@gmail.com>
2026-09-16 19:19:49 +0530

    Solved: The K Weakest Rows in a Matrix [Easy]

---
+++ 2D-Arrays/Easy/The K Weakest Rows in a Matrix/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int m = mat.length;
+        int arr[][] = new int [m][2];
+
+        for(int i=0; i<m; i++){
+            int count = 0;
+            for(int j=0; j<mat[i].length; j++){
+                count = count + mat[i][j];
+            }
+            arr[i][0] = count;
+            arr[i][1] = i;
+        }
+        Arrays.sort(arr, (a,b) -> { if(a[0]!=b[0]){
+            return a[0]-b[0];
+            }else{
+                return a[1]-b[1];
+            }
+        });
+        int ans[] = new int[k];
+        for(int i=0; i<k; i++){
+            ans[i] = arr[i][1];
+        }
+        return ans;
+    }
+}
