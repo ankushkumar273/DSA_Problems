76daf23448f09fc5e86bb51e5b6f3eecee20a0c2
ankushkumar273 <ankushkumar12102003@gmail.com>
2026-09-18 23:05:19 +0530

    Solved: While Loop [Easy]

---
+++ GeeksForGeeks/Easy/While Loop/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        // code here
+        for(int i=x; i>=0; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
